1391235570cc61fb78c06a15807281da81e15339
fersilverio <ssilveriofernando45@gmail.com>
2024-06-04 22:31:57 -0300

    Route login -> register

--- src/app/associate-login/associate-login.component.ts
+++ src/app/associate-login/associate-login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-associate-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './associate-login.component.html',
   styleUrl: './associate-login.component.css'
 })
