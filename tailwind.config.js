93b94333477482ccdca04350983a18f10e6d4088
fersilverio <ssilveriofernando45@gmail.com>
2024-06-04 21:27:34 -0300

    Login style structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,11 @@ module.exports = {
       },
       colors: {
         'git-gray-900': '#0e0e1a',
-        'git-gray-700': '#1a1a29'
+        'git-gray-700': '#1a1a29',
+        'clean-dark': '#0e0f13',
+        'clean-gray': '#16171c',
+        'gray-login-area-500': '#1c1d2f',
+        'gray-login-area-900': '#090a0e',
       }
     },
   },
