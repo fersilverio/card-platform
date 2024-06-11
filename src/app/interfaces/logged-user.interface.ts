91350ac8d4270c7f06ba9f79d07c19f836654866
fersilverio <ssilveriofernando45@gmail.com>
2024-06-10 22:10:17 -0300

    Creating initial login strucure

---
+++ src/app/interfaces/logged-user.interface.ts
@@ -0,0 +1,4 @@
+export interface ILoggedUser {
+  token: string,
+  name: string
+}
---
+++ src/app/services/user-login.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { ILoggedUser } from "../interfaces/logged-user.interface";
+import { tap } from "rxjs";
+
+@Injectable()
+export class UserLoginService {
+  constructor(private readonly httpClient: HttpClient) { }
+
+  login(email: string, password: string) {
+    return this.httpClient.post<ILoggedUser>("/login", { email: email, password: password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem("auth-token", value.token)
+        sessionStorage.setItem("auth-name", value.name)
+      })
+    )
+  }
+
+  logout() {
+    sessionStorage.removeItem("auth-token")
+    sessionStorage.removeItem("auth-name")
+  }
+}
