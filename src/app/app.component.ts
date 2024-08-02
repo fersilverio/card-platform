91d84095865d9b0cf9025d96ed4f5e054ec165c6
fersilverio <ssilveriofernando45@gmail.com>
2024-08-01 22:11:37 -0300

    Login reaching api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,13 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { AssociateProfileComponent } from './associate-profile/associate-profile.component';
 import { HomeComponent } from './home/home.component';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, NavbarComponent, FooterComponent, AssociateProfileComponent],
-  providers: [HttpClient],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent, FooterComponent, AssociateProfileComponent, HttpClientModule],
+  providers: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/services/user-login.service.ts
+++ src/app/services/user-login.service.ts
@@ -6,17 +6,19 @@ import { Router } from "@angular/router";
 
 @Injectable()
 export class UserLoginService {
-  constructor(private readonly httpClient: HttpClient, private readonly router: Router) { }
+  constructor(
+    private readonly httpClient: HttpClient,
+    private readonly router: Router
+  ) { }
 
   login(email: string | null | undefined, password: string | null | undefined) {
-    console.log("entrei aqui no login");
     return this.httpClient.post<ILoggedUser>("http://localhost:8080/auth/login", { email: email, password: password }).pipe(
       tap((value) => {
         sessionStorage.setItem("auth-token", value.token);
         sessionStorage.setItem("auth-name", value.name);
-        this.router.navigate(['/']);
+        this.router.navigate(['/profile']);
       })
-    )
+    ).subscribe();
   }
 
   logout() {
