bb0e1a1ca50f3176abe08bc0544670b11ef5a283
fersilverio <ssilveriofernando45@gmail.com>
2024-08-11 17:02:32 -0300

    ripple ui installed logout implemented

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { UserLoginService } from '../services/user-login.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,5 +10,17 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(private router: Router, private userLoginService: UserLoginService) { }
 
+  isLogged = false;
+
+  ngOnInit(): void {
+    this.isLogged = this.userLoginService.isLogged;
+  }
+
+  triggerLogout() {
+    this.userLoginService.logout();
+    this.router.navigate(['/login']);
+    this.isLogged = false;
+  }
 }
